0a23f0270ec792772f31b4586b28a0eb551b43b5
Ingrid Fosså <ingfo@stud.ntnu.no>
2020-10-05 22:29:21 +0200

    #3 Styling of login page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -5,19 +5,27 @@ class LoginPage extends Component<{}, {}> {
     render() {
         return (
             <div className={"login-page"}>
-                <h2>Logg inn</h2>
-                <form>
-                    <div className={"form-group username"}>
-                        <label className={"input-label"} htmlFor={"username"}>Brukernavn</label>
-                        <input name={"username"} className={"input"} autoFocus placeholder={"OlaNormann"}/>
-                    </div>
+                <div className={"login-section"}>
 
-                    <div className={"form-group password"}>
-                        <label className={"input-label"} htmlFor={"password"}>Passord</label>
-                        <input name={"password"} className={"input"} type={"password"} autoFocus
-                               placeholder={"Passord"}/>
-                    </div>
-                </form>
+
+                    <h2 className={"login-page-title"}>Logg inn</h2>
+                    <form className={"login-page-form"}>
+                        <div className={"form-group username"}>
+                            <label className={"input-label"} htmlFor={"username"}>Brukernavn</label>
+                            <input name={"username"} className={"input"} autoFocus placeholder={"OlaNormann"}/>
+                        </div>
+
+                        <div className={"form-group password"}>
+                            <label className={"input-label"} htmlFor={"password"}>Passord</label>
+                            <input name={"password"} className={"input"} type={"password"} autoFocus
+                                   placeholder={"Passord"}/>
+                        </div>
+
+                        <div className={"form-group"}>
+                            <button type={"submit"}>Logg inn</button>
+                        </div>
+                    </form>
+                </div>
 
             </div>
 
