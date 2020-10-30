8aef2c40b3456d6f40e8f8fe6856458696c765ee
Ingrid Fosså <ingfo@stud.ntnu.no>
2020-10-30 20:35:00 +0100

    #34 Added loader and 'no result', adjusted styling of stuff

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,8 +1,8 @@
-import React, { Component, FormEvent, useState } from "react";
-import { Input, Radio, Form, Accordion, Select } from "semantic-ui-react";
+import React, {Component, FormEvent, useState} from "react";
+import {Input, Radio, Form, Accordion, Select} from "semantic-ui-react";
 
 interface Props {
-  searchButtonClicked(input: string, titleType: string, orderDir: string): void;
+    searchButtonClicked(input: string, titleType: string, orderDir: string): void;
 }
 
 // This component reders the search bar.
@@ -10,105 +10,109 @@ interface Props {
 // with the state variables as arguments.
 // The searchButtonClicked function is passed on from Search page.
 const SearchBar = (props: Props) => {
-  const [inputState, setInputState] = useState<string>("");
-  const [orderDirState, setOrderDirState] = useState<string>("");
-  const [titleTypeState, setTitleTypeState] = useState<string>("");
+    const [inputState, setInputState] = useState<string>("");
+    const [orderDirState, setOrderDirState] = useState<string>("");
+    const [titleTypeState, setTitleTypeState] = useState<string>("");
 
-  // Functions to update component state when an input changes
-  const handleInputChange = (event: FormEvent<HTMLInputElement>) => {
-    setInputState(event.currentTarget.value);
-  };
+    // Functions to update component state when an input changes
+    const handleInputChange = (event: FormEvent<HTMLInputElement>) => {
+        setInputState(event.currentTarget.value);
+    };
 
-  const handleOrderDirChange = (event: FormEvent<HTMLInputElement>) => {
-    setOrderDirState(event.currentTarget.value);
-  };
+    const handleOrderDirChange = (event: FormEvent<HTMLInputElement>) => {
+        setOrderDirState(event.currentTarget.value);
+    };
 
-  const handleTitleTypeChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    setTitleTypeState(event.currentTarget.value);
-  };
+    const handleTitleTypeChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setTitleTypeState(event.currentTarget.value);
+    };
 
-  // Function that is called on form submit
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    props.searchButtonClicked(inputState, titleTypeState, orderDirState);
-  };
+    // Function that is called on form submit
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        props.searchButtonClicked(inputState, titleTypeState, orderDirState);
+    };
 
-  // Panels for the accoridon with extra search options
-  const panels = [
-    {
-      key: "advanced-search",
-      title: "Search Options",
-      content: {
-        content: (
-          <div>
-            <Form.Group inline>
-              <Form.Input disabled label="Order By" placeholder="Year" />
-              <div className="radio-div">
-                <label htmlFor="asc-radio">Ascending</label>
-                <input
-                  id="asc-radio"
-                  type="radio"
-                  value="ASC"
-                  checked={orderDirState === "ASC"}
-                  onChange={handleOrderDirChange}
-                />
-                <label htmlFor="desc-radio">Descending</label>
-                <input
-                  id="desc-radio"
-                  type="radio"
-                  value="DESC"
-                  checked={orderDirState === "DESC"}
-                  onChange={handleOrderDirChange}
-                />
-              </div>
-            </Form.Group>
-            <Form.Group>
-              <div className="field">
-                <label htmlFor="title-type-select">Title Type</label>
-                <select id="title-type-select" onChange={handleTitleTypeChange}>
-                  <option value=""></option>
-                  <option value="movie">Movie</option>
-                  <option value="tvShort">TV Short</option>
-                  <option value="tvMovie">TV Movie</option>
-                  <option value="short">Short</option>
-                  <option value="tvMiniSeries">TV Miniseries</option>
-                  <option value="videoGame">Video Game</option>
-                  <option value="tvEpisode">TV Episode</option>
-                  <option value="video">Video</option>
-                  <option value="tvSpecial">TV Special</option>
-                  <option value="tvSeries">TV Series</option>
-                </select>
-              </div>
-            </Form.Group>
-          </div>
-        ),
-      },
-    },
-  ];
+    // Panels for the accoridon with extra search options
+    const panels = [
+        {
+            key: "advanced-search",
+            title: "Search Options",
+            content: {
+                content: (
+                    <div id={"advanced-search-content"}>
+                        <Form.Group inline>
 
-  return (
-    <Form className={"search-bar"} onSubmit={handleSubmit}>
-      <Input
-        icon="search"
-        className={"search-input"}
-        autoFocus
-        onChange={handleInputChange}
-        tabIndex={0}
-        required
-      />
-      <Accordion defaultActiveIndex={-1} panels={panels} />
-      <button
-        className={"search-button"}
-        type={"submit"}
-        value={"submit"}
-        tabIndex={0}
-      >
-        Søk
-      </button>
-    </Form>
-  );
+                            <div className={"order-by-form"}>
+                                <label className={"form-label-bold"} htmlFor={"order-by-input"}>Order By:</label>
+                                <input id={"order-by-input"} placeholder={"Year"} disabled/>
+                            </div>
+                            <div className="radio-div">
+                                <label htmlFor="asc-radio">Ascending</label>
+                                <input
+                                    id="asc-radio"
+                                    type="radio"
+                                    value="ASC"
+                                    checked={orderDirState === "ASC"}
+                                    onChange={handleOrderDirChange}
+                                />
+                                <label htmlFor="desc-radio">Descending</label>
+                                <input
+                                    id="desc-radio"
+                                    type="radio"
+                                    value="DESC"
+                                    checked={orderDirState === "DESC"}
+                                    onChange={handleOrderDirChange}
+                                />
+                            </div>
+                        </Form.Group>
+                        <Form.Group>
+                            <div className="field">
+                                <label className={"form-label-bold"} htmlFor="title-type-select">Title Type:</label>
+                                <select id="title-type-select" onChange={handleTitleTypeChange}>
+                                    <option value=""></option>
+                                    <option value="movie">Movie</option>
+                                    <option value="tvShort">TV Short</option>
+                                    <option value="tvMovie">TV Movie</option>
+                                    <option value="short">Short</option>
+                                    <option value="tvMiniSeries">TV Miniseries</option>
+                                    <option value="videoGame">Video Game</option>
+                                    <option value="tvEpisode">TV Episode</option>
+                                    <option value="video">Video</option>
+                                    <option value="tvSpecial">TV Special</option>
+                                    <option value="tvSeries">TV Series</option>
+                                </select>
+                            </div>
+                        </Form.Group>
+                    </div>
+                ),
+            },
+        },
+    ];
+
+    return (
+        <Form className={"search-bar"} onSubmit={handleSubmit}>
+            <Input
+                icon="search"
+                className={"search-input"}
+                autoFocus
+                onChange={handleInputChange}
+                tabIndex={0}
+                required
+            />
+            <button
+                className={"search-button"}
+                type={"submit"}
+                value={"submit"}
+                tabIndex={0}
+            >
+                Søk
+            </button>
+            <Accordion className={"advanced-search"} defaultActiveIndex={-1} tabIndex={0} panels={panels}/>
+        </Form>
+    );
 };
 
 export default SearchBar;
--- client/src/components/SearchResult.tsx
+++ client/src/components/SearchResult.tsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import ResultTableAccordion from "./ResultTableAccordion";
 import {Movie} from "../types/DatabaseTypes";
 import HttpClient from "../modules/HttpClient";
+import {Loader} from "semantic-ui-react";
 
 
 interface Props {
@@ -26,8 +27,8 @@ class SearchResult extends Component<Props, State> {
             movies: [
                 {
                     // "Dummie-data" som vises når det søkes etter resultat for å unngå tom tabell.
-                    tconst: "1",
-                    title_type: "",
+                    tconst: "-1",
+                    title_type: "This is a placeholder for real data.",
                     primary_title: "Waiting for result",
                     original_title: "",
                     is_adult: false,
@@ -41,6 +42,7 @@ class SearchResult extends Component<Props, State> {
         };
 
         this.search = this.search.bind(this);
+        this.renderResultOrLoader = this.renderResultOrLoader.bind(this);
 
         if (
             this.props.searchInput != null &&
@@ -59,7 +61,7 @@ class SearchResult extends Component<Props, State> {
         // Dersom det ikkje er søkt på noko ennå vert det returnert og rendra ein tom div
         if (this.props.searchInput == null) {
             return (
-                <div className={"search-result no-search-result"}/>
+                <div className={"search-result nothing-searched"}/>
             )
         }
 
@@ -68,11 +70,32 @@ class SearchResult extends Component<Props, State> {
             return (
                 <div className={"search-result"}>
                     <h3>Søkeresultat:</h3>
-                    <ResultTableAccordion movies={this.state.movies}/>
+                    {this.renderResultOrLoader()}
                 </div>
             )
         }
     }
+
+    renderResultOrLoader() {
+        if (this.state.movies.length === 0) {
+            return (
+                <div className={"no-result"}>
+                    <p>No result for "{this.props.searchInput}"</p>
+                </div>
+            )
+        } else if (this.state.movies[0].tconst === "-1") {
+            return (
+                <div className={"result-loader"}>
+                    <Loader active inline={"centered"}/>
+                </div>
+            )
+        } else {
+            return (
+                <ResultTableAccordion movies={this.state.movies}/>
+            )
+        }
+    }
+
 
 // Search() tek inn argument for kva det skal søkast etter.
 // Spør databasen.
@@ -98,6 +121,7 @@ class SearchResult extends Component<Props, State> {
             this.setState({movies: response});
         });
     }
+
 }
 
 export default SearchResult;
