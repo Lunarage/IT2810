14b6db22ef868ae0e7c07f57e981395d46afd9bf
Molly Gibson <mollypg@stud.ntnu.no>
2020-11-14 15:35:07 +0100

    fixed small error in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
     // Setup Redux store and actions
     const dispatch = useDispatch();
     const loggedIn: boolean = useSelector((state: AppState ) => state.loggedIn);
-    const userName: string = useSelector((state: AppState ) => state.userName);
+    const userName = useSelector((state: AppState ) => state.userName);
 
     // Set up use of local storage
     const localStorage = new LocalStorageWrapper();
