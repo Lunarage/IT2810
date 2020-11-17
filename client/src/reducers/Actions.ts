af55c215791601be2e5f586127faa63b60e978e2
Molly Gibson <mollypg@stud.ntnu.no>
2020-11-17 10:16:07 +0100

    Formatting

--- client/src/reducers/Actions.ts
+++ client/src/reducers/Actions.ts
@@ -1,21 +1,20 @@
-
 export const toggle_loggedIn = (boolVal: boolean) => {
     return {
         type: "TOGGLE_LOGGED_IN",
-        payload: boolVal
-    } as const
+        payload: boolVal,
+    } as const;
 };
 
-export const set_username = ( text: string) => {
+export const set_username = (text: string) => {
     return {
         type: "SET_USERNAME",
-        payload: text
-    } as const
+        payload: text,
+    } as const;
 };
 
 export const logout = (boolVal: boolean) => {
     return {
         type: "LOGOUT",
-        payload: boolVal
-    } as const 
+        payload: boolVal,
+    } as const;
 };
--- client/src/reducers/Reducer.ts
+++ client/src/reducers/Reducer.ts
@@ -1,58 +1,57 @@
-import { toggle_loggedIn, logout, set_username } from "./Actions"
+import { toggle_loggedIn, logout, set_username } from "./Actions";
 import { createStore, Store } from "redux";
 
 type Actions =
-  | ReturnType<typeof toggle_loggedIn>
-  | ReturnType<typeof logout>
-  | ReturnType<typeof set_username>;
+    | ReturnType<typeof toggle_loggedIn>
+    | ReturnType<typeof logout>
+    | ReturnType<typeof set_username>;
 
+//Creates the types AppState
 export type AppState = {
     loggedIn: boolean;
     userName?: string | null;
 };
-
+//Sets initial root state for the app
 const initialAppState: AppState = {
     loggedIn: false,
     userName: null,
 };
-
-export const rootReducer = (state: AppState = initialAppState,  action: Actions) => {
+//Root reducer for the App, handels the states loggedIn/Loggedout and userName
+export const rootReducer = (
+    state: AppState = initialAppState,
+    action: Actions
+) => {
     switch (action.type) {
-        case 'TOGGLE_LOGGED_IN':
+        case "TOGGLE_LOGGED_IN":
             return {
                 ...state,
                 loggedIn: action.payload,
             };
-        
-        case 'SET_USERNAME':
+
+        case "SET_USERNAME":
             return {
                 ...state,
-                userName: action.payload
+                userName: action.payload,
             };
-        
-        case 'LOGOUT':
+
+        case "LOGOUT":
             return {
                 ...state,
                 loggedIn: action.payload,
-                userName: null
+                userName: null,
             };
         default:
-            return state
+            return state;
     }
 };
-
+//Creating store
 function configureStore(): Store<AppState> {
     return createStore(
-      rootReducer,
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+        rootReducer,
+        (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+            (window as any).__REDUX_DEVTOOLS_EXTENSION__()
     );
-  }
+}
 
-export const store = configureStore()
-
-console.log(store.getState)
-
-
-/* These sites are used as src for this code: 
-https://codesandbox.io/s/github/reduxjs/redux-essentials-counter-example/tree/master/?from-embed=&file=/src/features/counter/counterSlice.js:49-83
-https://redux.js.org/tutorials/essentials/part-2-app-structure */
+//Oppretter en store
+export const store = configureStore();
