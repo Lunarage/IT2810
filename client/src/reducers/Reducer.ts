80e371a8947906a0e7ddac623438678a2769a00a
Magne Halvorsen <mh95244719@gmail.com>
2020-11-17 12:52:29 +0100

    (#4) Search state is now stored in redux

--- client/src/reducers/Reducer.ts
+++ client/src/reducers/Reducer.ts
@@ -1,20 +1,53 @@
-import { toggle_loggedIn, logout, set_username } from "./Actions";
+import {
+    toggle_loggedIn,
+    logout,
+    set_username,
+    setStoreSearchState,
+} from "./Actions";
 import { createStore, Store } from "redux";
+import { Movie } from "../types/DatabaseTypes";
 
 type Actions =
     | ReturnType<typeof toggle_loggedIn>
     | ReturnType<typeof logout>
-    | ReturnType<typeof set_username>;
+    | ReturnType<typeof set_username>
+    | ReturnType<typeof setStoreSearchState>;
+
+// The type for holding search information and result
+export type StoreSearchState = {
+    inputSearch: string;
+    inputOrderDir: "ASC" | "DESC";
+    inputTitleType: string;
+    pageActivePage: number;
+    pageTotalPages: number;
+    pageDisabled: boolean;
+    searchStatus: "none" | "waiting" | "success" | "failure";
+    searchErrorMessage: string | null;
+    searchMovies: Movie[];
+};
 
 //Creates the types AppState
 export type AppState = {
     loggedIn: boolean;
     userName?: string | null;
+    searchState: StoreSearchState;
 };
+
 //Sets initial root state for the app
 const initialAppState: AppState = {
     loggedIn: false,
     userName: null,
+    searchState: {
+        inputSearch: "",
+        inputOrderDir: "ASC",
+        inputTitleType: "",
+        pageActivePage: 1,
+        pageTotalPages: 2,
+        pageDisabled: false,
+        searchStatus: "none",
+        searchErrorMessage: null,
+        searchMovies: [],
+    },
 };
 //Root reducer for the App, handels the states loggedIn/Loggedout and userName
 export const rootReducer = (
@@ -40,6 +73,11 @@ export const rootReducer = (
                 loggedIn: action.payload,
                 userName: null,
             };
+        case "SET_SEARCH_STATE":
+            return {
+                ...state,
+                searchState: action.payload,
+            };
         default:
             return state;
     }
@@ -53,5 +91,5 @@ function configureStore(): Store<AppState> {
     );
 }
 
-//Create and exports store 
+//Create and exports store
 export const store = configureStore();
